521c3867b8d38cf42c69e8cc3fdcdc6a40a3efd2
issasahily <issasahily20@gmail.com>
2023-05-29 17:20:52 +0300

    dfgg

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,19 +10,19 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-
+path(route = 'about/', view = views.about, name='about'),
     # path for contact us view
-
+path(route = 'contact/', view = views.contact, name='contact'),
     # path for registration
-
+path('signup/', views.registration_request, name='registration'),
     # path for login
-
+ path('login/', views.login_request, name='login'),
     # path for logout
-
+ path('logout/', views.logout_request, name='logout'),
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
 
-    # path for add a review view
+    # path for add a review view sdsdsdasdasdasdererdfd
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
